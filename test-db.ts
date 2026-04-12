21ae9c120dd511856661a07cb6e4606dcb3d1172
Franco Benedetti <franco@growthhq.net>
2026-04-12 15:16:08 +0200

    chore: remove unused test-db.ts script

--- test-db.ts
+++
@@ -1,3 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-const supa = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
-supa.from('publications').select('source_url').then(console.log);
