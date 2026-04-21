395b0f6b3d4b99b1234f0a6f2f582ecf06a2328e
Franco Benedetti <franco@growthhq.net>
2026-04-21 10:22:02 +0200

    feat: add support for email OTP verification to authentication callback route

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,27 +1,45 @@
 import { NextResponse } from 'next/server'
 // The client you created in Step 2
 import { createClient } from '@/utils/supabase/server'
+import { type EmailOtpType } from '@supabase/supabase-js'
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url)
   const code = searchParams.get('code')
+  const token_hash = searchParams.get('token_hash')
+  const type = searchParams.get('type') as EmailOtpType | null
   // if "next" is in search params, use it as the redirection URL
   const next = searchParams.get('next') ?? '/dashboard'
 
-  if (code) {
-    const supabase = await createClient()
+  const supabase = await createClient()
+
+  let isSuccess = false;
+
+  if (token_hash && type) {
+    const { error } = await supabase.auth.verifyOtp({
+      type,
+      token_hash,
+    })
+    if (!error) {
+      isSuccess = true;
+    }
+  } else if (code) {
     const { error } = await supabase.auth.exchangeCodeForSession(code)
     if (!error) {
-      const forwardedHost = request.headers.get('x-forwarded-host') // LDAP etc.
-      const isLocalEnv = process.env.NODE_ENV === 'development'
-      if (isLocalEnv) {
-        // we can be sure that there is no proxy between localhost and us
-        return NextResponse.redirect(`${origin}${next}`)
-      } else if (forwardedHost) {
-        return NextResponse.redirect(`https://${forwardedHost}${next}`)
-      } else {
-        return NextResponse.redirect(`${origin}${next}`)
-      }
+      isSuccess = true;
+    }
+  }
+
+  if (isSuccess) {
+    const forwardedHost = request.headers.get('x-forwarded-host') // LDAP etc.
+    const isLocalEnv = process.env.NODE_ENV === 'development'
+    if (isLocalEnv) {
+      // we can be sure that there is no proxy between localhost and us
+      return NextResponse.redirect(`${origin}${next}`)
+    } else if (forwardedHost) {
+      return NextResponse.redirect(`https://${forwardedHost}${next}`)
+    } else {
+      return NextResponse.redirect(`${origin}${next}`)
     }
   }
 
